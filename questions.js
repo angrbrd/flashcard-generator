27b083cf8c713b63018cb891674dd4171e21e0bd
Anna D Derbakova <hiopa16@gmail.com>
2017-02-27 23:56:18 -0500

    Adding questions list
    Adding a list of 18 questions, containing the full text, the cloze
    deletion, and the close-deleted text.

---
+++ questions.js
@@ -0,0 +1,92 @@
+exports.questions = [
+	{
+		full: 'World War I began in 1914, shortly after the assassination of Arch-Duke Ferdinand of the Austro-Hungarian empire.',
+		cloze: '1914',
+		partial: 'World War I began in ..., shortly after the assassination of Arch-Duke Ferdinand of the Austro-Hungarian empire.'
+	},
+	{
+		full: 'Adolf Hitler was born in the city of Lintz in Austria, near the German border.',
+		cloze: 'Austria',
+		partial: 'Adolf Hitler was born in the city of Lintz in ..., near the German border.'
+	},
+	{
+		full: 'JFK was assassinated while driving through Dallas in November 1963.',
+		cloze: 'Dallas',
+		partial: 'JFK was assassinated while driving through ... in November 1963.'
+	},
+	{
+		full: 'Andrew Jackson was the commanding general of the Battle of New Orleans in 1815.',
+		cloze: 'Jackson',
+		partial: 'Andrew ... was the commanding general of the Battle of New Orleans in 1815.'
+	},
+	{
+		full: 'Douglas MacArthur famously stated "I shall return" after leaving Bataan in 1942 fleeing from the conquering Japanese. He in fact did return.',
+		cloze: 'MacArthur',
+		partial: 'Douglas ... famously stated "I shall return" after leaving Bataan in 1942 fleeing from the conquering Japanese. He in fact did return.'
+	},
+	{
+		full: 'The American involvement in the Korean war lasted from 1950 to 1953.',
+		cloze: '1953',
+		partial: 'The American involvement in the Korean war lasted from 1950 to ... .'
+	},
+	{
+		full: 'The Battle of Hastings in 1066 was fought in England.',
+		cloze: 'England',
+		partial: 'The Battle of Hastings in 1066 was fought in ... .'
+	},
+	{
+		full: 'The Magna Carta was created in 1215 issued by King John I of England. It was a document that signed away much of the royal power and is one of the first major political documents.',
+		cloze: 'Magna Carta',
+		partial: 'The ... was created in 1215 issued by King John I of England. It was a document that signed away much of the royal power and is one of the first major political documents.'
+	},
+	{
+		full: 'Gutenberg (c. 1400 – February 3, 1468) was a German goldsmith and printer who is credited with inventing movable type printing in Europe around 1439, and mechanical printing globally.',
+		cloze: 'Gutenberg',
+		partial: '... (c. 1400 – February 3, 1468) was a German goldsmith and printer who is credited with inventing movable type printing in Europe around 1439, and mechanical printing globally.'
+	},
+	{
+		full: 'The Bubonic Plague was carried by fleas on rats aboard trading ships from the East and ravaged Europe for many centuries.',
+		cloze: 'Plague',
+		partial: 'The Bubonic ... was carried by fleas on rats aboard trading ships from the East and ravaged Europe for many centuries.'
+	},
+	{
+		full: 'The Hundred Years War was fought between England and France.',
+		cloze: 'France',
+		partial: 'The Hundred Years War was fought between England and ... .',
+	},
+	{
+		full: 'The Roman Emperor Hadrian built a massive wall across Northern Britain in 122 A.D.',
+		cloze: 'Hadrian',
+		partial: 'The Roman Emperor ... built a massive wall across Northern Britain in 122 A.D.'
+	},
+	{
+		full: 'Martin Luther, concerned over the indulgence policy of the Catholic church would become the father of the Protestant movement in Europe.',
+		cloze: 'Luther',
+		partial: 'Martin ..., concerned over the indulgence policy of the Catholic church would become the father of the Protestant movement in Europe.',
+	},
+	{
+		full: 'In 1594 Shakespeare became an actor and a playwright in the company of the Globe Theatre.',
+		cloze: 'Globe',
+		partial: 'In 1594 Shakespeare became an actor and a playwright in the company of the ... Theatre.',
+	},
+	{
+		full: 'The Khmer Rouge was a regime ruling the country of Cambodia in the 20th century.',
+		cloze: 'Cambodia',
+		partial: 'The Khmer Rouge was a regime ruling the country of ... in the 20th century.',
+	},
+	{
+		full: 'Atilla the Hun was a famous 5th century A.D conqueror who was known as "The Scourge of God"',
+		cloze: 'Atilla',
+		partial: '... the Hun was a famous 5th century A.D conqueror who was known as "The Scourge of God"',
+	},
+	{
+		full: 'The Winchester model 1873 lever action rifle was a revolutionary firearm that is known as "The Gun that Won the West".',
+		cloze: 'Winchester',
+		partial: 'The ... model 1873 lever action rifle was a revolutionary firearm that is known as "The Gun that Won the West".',
+	},
+	{
+		full: 'Marco Polo was the first Western explorer to reach China.',
+		cloze: 'Polo',
+		partial: 'Marco ... was the first Western explorer to reach China.'
+	}
+]
